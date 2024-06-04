4e8598c9a304818a53c6738ac94af9b36673e8d9
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-05 00:20:12 +0200

    adding repositories and config mock/web

--- lib/repository/restaurants/restaurants_repository.dart
+++ lib/repository/restaurants/restaurants_repository.dart
@@ -2,5 +2,5 @@ import 'package:htc_flutter/models/restaurant.dart';
 
 abstract class RestaurantsRepository {
   Future<Restaurant?> getOne(int id);
-  Future<List<Restaurant>?> getAll(int id);
+  Future<List<Restaurant>?> getAll();
 }
---
+++ lib/repository/restaurants/restaurants_repository_web.dart
@@ -0,0 +1,16 @@
+import 'package:htc_flutter/models/restaurant.dart';
+import 'package:htc_flutter/repository/restaurants/restaurants_repository.dart';
+
+class RestaurantsRepositoryWeb implements RestaurantsRepository {
+  @override
+  Future<List<Restaurant>?> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Restaurant?> getOne(int id) {
+    // TODO: implement getOne
+    throw UnimplementedError();
+  }
+}
