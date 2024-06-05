a879a437593a489b1f8707e21ef1d87bac9e0b7b
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-05 17:48:34 +0200

    creating space to design

---
+++ lib/screens/story_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StoryScreen extends StatefulWidget {
+  const StoryScreen({super.key});
+
+  @override
+  State<StoryScreen> createState() => _StoryScreenState();
+}
+
+class _StoryScreenState extends State<StoryScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times: ${const String.fromEnvironment('APP_ENV')}',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const Card(
+              child: Row(children: [Text("test")]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/themes/text_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const bodyLargeStyle = TextStyle(fontSize: 32, color: Colors.white);
+const bodyMediumStyle = TextStyle(fontSize: 24, color: Colors.white);
+const bodySmallStyle = TextStyle(fontSize: 16, color: Colors.white);
