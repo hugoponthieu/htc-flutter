0134b8a6db1d43759246d57aa982c9ab690e5a31
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-08 00:34:36 +0200

    feat(home): display meals by restaurant

--- lib/widgets/restaurant_card.dart
+++ lib/widgets/restaurant_card.dart
@@ -3,24 +3,20 @@ import 'package:htc_flutter/models/meal.dart';
 import 'package:htc_flutter/models/restaurant.dart';
 import 'package:htc_flutter/themes/text_theme.dart';
 import 'package:htc_flutter/widgets/arrow_button.dart';
-
+import 'package:htc_flutter/widgets/expansion_meal_list.dart';
 
 class RestaurantCard extends StatelessWidget {
   final void Function(int) navigateToDetail;
   final Future<List<Meal>?> Function(int id) getRestaurantMeals;
   final Future<List<Meal>?> futureMeals;
   final Restaurant restaurant;
-  final ExpansionTileController tileController;
-  final List<ExpansionTileController> tileControllers;
   RestaurantCard(
       {super.key,
       required this.navigateToDetail,
       required this.restaurant,
       required this.getRestaurantMeals})
-      : futureMeals = getRestaurantMeals(restaurant.id),
-        tileControllers =
-            List.generate(meals.length, (index) => ExpansionTileController()),
-        tileController = ExpansionTileController();
+      : futureMeals = getRestaurantMeals(restaurant.id);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -28,26 +24,10 @@ class RestaurantCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           restaurantTopTile(navigateToDetail),
-          Container(
-              constraints: const BoxConstraints.tightFor(width: 300),
-              child: Column(
-                  children: meals
-                      .asMap()
-                      .entries
-                      .map((entry) => mealExpansionTile(
-                          entry.value, tileControllers[entry.key], entry.key))
-                      .toList()))
+          ExpansionMealList(futureMeals: futureMeals)
         ],
       ),
     );
-  }
-
-  void collapseAllTiles(int controllerKey) {
-    for (var entry in tileControllers.asMap().entries) {
-      if (entry.key != controllerKey) {
-        if (entry.value.isExpanded) entry.value.collapse();
-      }
-    }
   }
 
   Widget restaurantTopTile(void Function(int) navigateToDetail) {
@@ -60,43 +40,4 @@ class RestaurantCard extends StatelessWidget {
       onTap: () => navigateToDetail(1),
     );
   }
-
-  Widget mealExpansionTile(
-      Meal meal, ExpansionTileController tileController, int controllerKey) {
-    return ExpansionTile(
-        controller: tileController,
-        onExpansionChanged: (value) {
-          if (tileController.isExpanded) collapseAllTiles(controllerKey);
-        },
-        maintainState: true,
-        controlAffinity: ListTileControlAffinity.trailing,
-        title: Text(meal.type, style: bodyMediumStyle),
-        children: foodyDisplay(meal));
-  }
-
-  List<Widget> foodyDisplay(Meal meal) {
-    return [
-      ...meal.foodies.map(
-        (foody) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Column(
-              children: [
-                Text(
-                  "--${foody.type}--",
-                  style: bodySmallStyle.copyWith(fontWeight: FontWeight.bold),
-                ),
-                ...foody.content.map(
-                  (food) {
-                    return Text(food, style: bodySmallStyle);
-                  },
-                )
-              ],
-            ),
-          );
-        },
-      )
-    ];
-  }
 }
-
